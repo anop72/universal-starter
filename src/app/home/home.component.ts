0a3827724b57f85401fed17f963e7bc108d81679
ak <anop72@gmail.com>
2016-06-28 09:24:03 +0700

    reference html file to separate css
    - create simple sass file
    - change how to reference style by using `styles` instead of `styleUrls`
    - add tag link stylesheet to separate css file in html

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+require('./home.component.scss')
 
 @Component({
   moduleId: __filename,
   selector: 'home',
-  styleUrls: [
-    'home.style.css'
-  ],
+  // styleUrls: [
+  //   'home.style.css'
+  // ],
+  styles: ['home.component.scss'],
   templateUrl: 'home.template.html'
 })
 export class Home {
