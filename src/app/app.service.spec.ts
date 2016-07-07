a49e60b414fad20515a5e3161540f5bada4ec49a
ak <anop72@gmail.com>
2016-07-07 23:41:20 +0700

    simple service spec file and testing for inject DI

---
+++ src/app/app.service.spec.ts
@@ -0,0 +1,23 @@
+import {
+  beforeEachProviders,
+  describe,
+  expect,
+  it,
+  inject
+} from '@angular/core/testing';
+
+import { provide } from '@angular/core';
+import { Service } from './app.service';
+
+describe('Service:', () => {
+
+  beforeEachProviders(() => {
+    return [Service];
+  });
+
+  it('should create the service',
+      inject([Service], (service: Service) => {
+          expect(service).toBeTruthy();
+  }));
+
+});
