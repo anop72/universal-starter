6045a120adf3f9b4079ae48fcf402b65e0f834ea
ak <anop72@gmail.com>
2016-09-08 16:25:07 +0700

    server env

--- src/server.ts
+++ src/server.ts
@@ -58,6 +58,6 @@ app.get('*', function(req, res) {
 });
 
 // Server
-app.listen(3000, () => {
-  console.log('Listening on: http://localhost:3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Listening on: http://localhost:${server.address().port}');
 });
