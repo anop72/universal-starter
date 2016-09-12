46f8813c955ed4aec2e85e68e1380504251e3ba6
ak <anop72@gmail.com>
2016-09-12 12:10:12 +0700

    add decralation for about to remove warning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule }       from '@angular/core';
 import { BrowserModule  } from '@angular/platform-browser';
 import { HttpModule  } from '@angular/http';
 import { RouterModule  } from '@angular/router';
-import { App }   from './app.component';
+import { App, About }   from './app.component';
 import { routes }   from './app.routes';
 import { Home } from './home';
 
 @NgModule({
-    declarations: [App, Home],
+    declarations: [App, Home, About],
     imports:  [
       BrowserModule
       , RouterModule.forRoot(routes)
